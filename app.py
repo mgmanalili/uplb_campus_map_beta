e9ea7e8d066e4bdb2f6c4a5941789c8825acce33
mgmanalili <michaelandrew.manalili@gmail.com>
2018-09-08 14:26:23 +0200

    HILAGA UPLB campus map Beta

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def uplb_sv():
+    return render_template('uplb_sv.html')
+
+if __name__ == '__main__':
+    app.run(port=8000,
+	    use_reloader=True,
+	    debug=True)
+
+
